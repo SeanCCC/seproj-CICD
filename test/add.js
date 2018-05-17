03146ecd59cab3800d7adddf4f1096eebe67d181
seanccc <sean831004@gmail.com>
2018-05-18 03:28:30 +0800

    change test

--- test/add.js
+++ test/add.js
@@ -5,13 +5,13 @@ describe('#add', () => {
     // 測試算出來的平均是不是 2.5
     it('should return the num+1', done => {
       var result = add(-10)
-      result.should.equal(-9)
+      result.should.equal(-8)
       done()
     })
     // 測試有沒有回傳 NaN
     it('should return NaN when the number is not natural', done => {
-      var result = add(10.5)
-      result.should.equal(11)
+      var result = add(10)
+      result.should.equal(12)
       done()
     })
   })
