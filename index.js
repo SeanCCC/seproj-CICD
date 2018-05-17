55e0b193e8c75dd6595b3e860d72e8a8475a1144
seanccc <sean831004@gmail.com>
2018-05-17 15:47:35 +0800

    basic API done

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var add = require('./lib/add');
+var app = express();
+
+app.get('/', function (req, res) {
+    var data = {
+        "msg": "hello world!", 
+        "result": add(Number(req.query.input))
+    }
+    console.log( data );
+    res.end( JSON.stringify(data) );
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("應用實例，訪問地址為 http://%s:%s", host, port)
+
+})
