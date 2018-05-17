fc7d44ffa3c9874aa9731a59a139752b87b003dc
seanccc <sean831004@gmail.com>
2018-05-17 23:20:17 +0800

    minor fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/test', function (req, res) {
     res.end( JSON.stringify(data) );
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
 
   var host = server.address().address
   var port = server.address().port
